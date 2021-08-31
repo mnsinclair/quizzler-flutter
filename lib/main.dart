886af72332e535fe30049016cc0a54c2fdc019d2
Matt Sinclair <msin553@aucklanduni.ac.nz>
2021-08-31 17:24:14 +1200

    Initial commit: basic quiz functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,73 +26,129 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
+  ];
+  int questionIndex = 0;
+
+  void addAnswerToScoreKeeper({Question question, bool buttonClicked}) {
+    Icon icon;
+    if (buttonClicked == question.answer) {
+      icon = Icon(
+        Icons.check,
+        color: Colors.green,
+      );
+    } else {
+      icon = Icon(
+        Icons.close,
+        color: Colors.red,
+      );
+    }
+    scoreKeeper.add(icon);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
+    Widget result;
+    if(0 <= questionIndex && questionIndex < questions.length){
+      result = Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Center(
+                child: Text(
+                  questions[questionIndex].text,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: FlatButton(
+                textColor: Colors.white,
+                color: Colors.green,
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
                 ),
+                onPressed: () {
+                  //The user picked true.
+                  setState(() {
+                    addAnswerToScoreKeeper(question: questions[questionIndex], buttonClicked: true);
+                    questionIndex++;
+                  });
+                },
               ),
-              onPressed: () {
-                //The user picked true.
-              },
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: FlatButton(
+                color: Colors.red,
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
                 ),
+                onPressed: () {
+                  //The user picked false.
+                  setState(() {
+                    addAnswerToScoreKeeper(question: questions[questionIndex], buttonClicked: false);
+                    questionIndex++;
+                  });
+
+                },
               ),
-              onPressed: () {
-                //The user picked false.
-              },
             ),
           ),
-        ),
-        //TODO: Add a Row here as your score keeper
-      ],
-    );
+          Row(
+            children: scoreKeeper,
+          ),
+        ],
+      );
+    } else {
+      result = Center(
+        child: Text("You are finished!")
+      );
+    }
+    return result;
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
+class Check extends StatelessWidget {
+  const Check({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
+
+class Cross extends StatelessWidget {
+  const Cross({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      Icons.close,
+      color: Colors.red,
+    );
+  }
+}
---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+
+  String text;
+  bool answer;
+
+  Question({String q, bool a}){
+    text = q;
+    answer = a;
+  }
+
+}
